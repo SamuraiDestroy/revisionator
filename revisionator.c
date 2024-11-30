999294069f7f3f596e85c88689e44d1111b1f9be
SamuraiDestroy <109217185+SamuraiDestroy@users.noreply.github.com>
2024-11-30 16:52:53 +0100

    Added some stuff

--- revisionator.c
+++ revisionator.c
@@ -1,1 +1,19 @@
 #include <gtk/gtk.h>
+
+static void window(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window;
+    GtkWidget *button;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW (window), "REVISIONATOR");
+    gtk_window_set_default_size(GTK_WINDOW (window), 800, 800);
+
+    gtk_window_preset(GTK_WINDOW (window));
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+}
